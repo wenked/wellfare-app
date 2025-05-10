940739ac79bbbb5ec6ec32d077649e2adf69c6e2
Matheus Santos Araújo <wenked.matheus@yahoo.com.br>
2025-05-10 18:41:33 -0300

    remove files

--- src/Pages/AuthPage.tsx
+++ src/Pages/AuthPage.tsx
@@ -42,7 +42,7 @@ const AuthPage: React.FC = () => {
 			}
 		}
 	};
-	console.log('testr');
+
 	return (
 		<div className="min-h-screen flex flex-col items-center justify-center bg-slate-100 py-12 px-4 sm:px-6 lg:px-8">
 			<div className="max-w-md w-full space-y-8 bg-white p-8 sm:p-10 rounded-xl shadow-md">
